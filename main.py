7b2719fce32e264ce6fb003e446d7a02a8eced4f
Dylan Berry <dylanjberry@icloud.com>
2026-05-04 17:46:12 -0700

    broken actually

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ def main():
     print(sys.version[2])
     assert 6 == 6
     assert 9 == 9
+    
+    awesomeCheckOut = 7
+    
+    awesomeCheckOut2 = 7
+    awesomeCheckOut3 = 7
 
 
 if __name__ == "__main__":
